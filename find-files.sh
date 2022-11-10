a725416c19361620c6fa1f14216a8787002dab2d
Mathias Wöß <mat.woess@gmail.com>
2022-11-10 18:25:27 +0100

    small improvement to script

--- find-files.sh
+++ find-files.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 function help() {
   echo "Usage: $(basename $0) DIRECTORY
   List all *file* paths in DIRECTORY recursively.
@@ -6,8 +8,15 @@ function help() {
 
 if [[ $# -ne 1 ]]; then
   echo "ERROR: invalid number of arguments!" >&2
-  help
+  echo "" >&2
+  help >&2
   exit 1
 fi
 
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+  help
+  exit 0
+fi
+
+# find all files in directory $1 that are of type file and do not start with "."
 find $1 -not -path '*/.*' -type f
